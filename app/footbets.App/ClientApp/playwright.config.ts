606e7a3587008202d29fb6f861aa768d52a03e2e
SAMPAN Blehoue Hermann Raphael <raphaelsampan@gmail.com>
2025-03-31 12:35:50 +0200

    chore(front-ci): add frontend ci and build config

--- app/footbets.App/ClientApp/playwright.config.ts
+++ app/footbets.App/ClientApp/playwright.config.ts
@@ -15,14 +15,15 @@ export default defineConfig({
   testDir: './src/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
+  globalTimeout: process.env.CI ? 60 * 60 * 1000 : undefined,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
